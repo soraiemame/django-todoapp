86026a82f5607735e7ee018ccc9d174745227148
soraiemame <funasou46@yahoo.co.jp>
2021-07-24 10:23:55 +0900

    fix

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^a=sr96po4x=4j7o%x5j^1vwup@=0479@npbf)#42vkyxu-_r4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
